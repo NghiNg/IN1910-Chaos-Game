9f99c46b3109a628f3372dd40524cbe44ef66d80
bdnguye <bdnguye@student.matnat.uio.no>
2018-11-15 16:58:16 +0100

    Fixed point functions

--- triangle.py
+++ triangle.py
@@ -7,7 +7,7 @@ x,y,z = c
 
 plt.scatter([0,0,np.sqrt(3)/2],[0,1,1./2])
 plt.axis('equal')
-#plt.show()
+plt.show()
 
 def r(n):
     '''Creates a list of random numbers between [0,1).'''
@@ -21,24 +21,37 @@ def r(n):
         r[i] = r[i]/sum
     return r
 
-def points(n):
+def point():
     R = r(3)
-    points = []
     a = []
+    x = 0
+    y = 0
+    cc = np.copy(c)
+    for i in range(3):
+        for j in range(2):
+            #a.append(R[i]*c[i][j])
+            cc[i][j] = R[i]*c[i][j]
+    for i in cc:
+        x += i[0]
+        y += i[1]
+        point = [x,y]
+    return point
+
+def points(n):
+    points = []
     for i in range(n):
-        s = 0
-        for j in range(3):
-            a.append([[r[j]*k for k in l] for l in c])
-        points.append(s)
+        points.append(point())
     return points
+print(point())
+print(points(2))
 
 x = []
 y = []
-points(2)
-"""P = points(1000)
+'''
+P = points(1000)
 for i in range(1000):
     x.append(P[i][0])
     y.append(P[i][1])
 plt.scatter(x,y)
 plt.show()
-"""
+'''
