89175f0e208b2f0d39d1c5acd33f59b721c2d873
bdnguye <bdnguye@student.matnat.uio.no>
2018-11-28 18:51:46 +0100

    Dunno.

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 import numpy as np
-a = []
-theta = 2*np.pi/4
-theatre = [theta]
-for i in range(3):
-    theatre.append(theatre[i]+theta)
-for i in range(4):
-    a.append([np.sin(theatre[i]), np.cos(theatre[i])])
-a = np.array(a)
+a = [0,1]
+b = 3.2
+for i in range(2):
+    a[i] *= b
 print(a)
