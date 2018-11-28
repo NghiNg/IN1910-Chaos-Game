4b6875b35d5d086722a21545865331fecb088e41
bdnguye <bdnguye@student.matnat.uio.no>
2018-11-28 19:03:08 +0100

    3b done, easy.

--- fern.py
+++ fern.py
@@ -14,7 +14,8 @@ class AffineTransform(object):
     def __call__(self,x,y):
         point = [self.a*x + self.b*y + self.e, self.c*x + self.d*y + self.f]
         return point
-'''
-a = AffineTransform(1,2,3,4,5,6)
-print(a.__call__(1,2))
-'''
+
+f1 = AffineTransform(c = 0.16)
+f2 = AffineTransform(0.85, 0.04, -0.04, 0.85, 0, 1.60)
+f3 = AffineTransform(0.20, -0.26, 0.23, 0.22, 0, 1.60)
+f4 = AffineTransform(-0.15, 0.28, 0.26, 0.24, 0, 0.44)
